e7173febcd0d4dfe4189b8810c8f6a011da5b5b3
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-04-23 22:39:51 -0600

    Init project

---
+++ src/test/java/com/training/gradesubmission/gradesubmission/GradesubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.gradesubmission.gradesubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradesubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
