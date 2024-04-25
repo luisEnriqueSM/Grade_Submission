fa3d9902a52a9c66c45ab973788f90796200389c
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-04-24 21:21:34 -0600

    Enable Lombok in POJO classes and remove Getters and Setters

--- src/main/java/com/training/gradesubmission/entity/Grade.java
+++ src/main/java/com/training/gradesubmission/entity/Grade.java
@@ -1,24 +1,17 @@
 package com.training.gradesubmission.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Grade {
 
     private Long id;
     private String score;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getScore() {
-        return this.score;
-    }
-
-    public void setScore(String score) {
-        this.score = score;
-    }
 
 }
--- src/main/java/com/training/gradesubmission/entity/Student.java
+++ src/main/java/com/training/gradesubmission/entity/Student.java
@@ -1,11 +1,19 @@
 package com.training.gradesubmission.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "student")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 
     @Id
@@ -16,29 +24,5 @@ public class Student {
     private String name;
     @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getBirthDate() {
-        return this.birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
 
 }
